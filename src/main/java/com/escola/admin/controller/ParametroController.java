273fa8a1d69421aabfddf6f78120c3ebb45e78cc
Rogério Cardoso <rogerio.cardoso@trt18.jus.br>
2025-07-14 14:35:24 -0300

    gravar configuração

--- src/main/java/com/escola/admin/controller/ParametroController.java
+++ src/main/java/com/escola/admin/controller/ParametroController.java
@@ -1,8 +1,8 @@
 package com.escola.admin.controller;
 
 
-import com.escola.admin.model.entity.Parametro;
 import com.escola.admin.model.mapper.ParametroMapper;
+import com.escola.admin.model.request.ParametroRequest;
 import com.escola.admin.model.response.ParametroResponse;
 import com.escola.admin.service.ParametroService;
 import lombok.AccessLevel;
@@ -10,11 +10,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-
-import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -29,8 +28,18 @@ public class ParametroController {
     @QueryMapping
     public ParametroResponse findByChave(@Argument String chave) {
         log.info("Executando findByChave com a chave: {}", chave);
-        Optional<Parametro> optParametro = service.findByChave(chave);
+        var optParametro = service.findByChave(chave);
         return optParametro
+                .map(mapper::toResponse) // Se o Optional contiver um valor, aplica o mapper
+                .orElse(null);
+    }
+
+    @MutationMapping
+    @PreAuthorize("isAuthenticated()")
+    public ParametroResponse salvarParametro(@Argument ParametroRequest request) {
+        log.info("Executando salvarParametro salvar: {}", request);
+        var entity = service.salvar(request);
+        return entity
                 .map(mapper::toResponse) // Se o Optional contiver um valor, aplica o mapper
                 .orElse(null);
     }
