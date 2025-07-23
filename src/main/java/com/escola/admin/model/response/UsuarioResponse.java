cc8fdc01b9086182a9bd71bd752afc4348fbdada
Rogério Cardoso <rogerio.cardoso@trt18.jus.br>
2025-07-23 15:21:43 -0300

    crud usuario

--- src/main/java/com/escola/admin/model/response/UsuarioResponse.java
+++ src/main/java/com/escola/admin/model/response/UsuarioResponse.java
@@ -1,8 +1,10 @@
 package com.escola.admin.model.response;
 
 import com.escola.admin.model.entity.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public record UsuarioResponse(
@@ -15,7 +17,11 @@ public record UsuarioResponse(
         boolean enabled,
         @JsonInclude(JsonInclude.Include.ALWAYS)
         EmpresaResponse empresa,
-        Set<Role> roles
+        Set<Role> roles,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDateTime dataCadastro,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDateTime dataAtualizacao
 
 ) {
 }
